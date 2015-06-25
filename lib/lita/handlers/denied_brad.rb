45cdebcbc07c799f823436938e456c2dca2fc959
Rudikza <rudi.kramer@gmail.com>
2015-06-25 15:11:40 +0200

    Fixed brackets

--- lib/lita/handlers/denied_brad.rb
+++ lib/lita/handlers/denied_brad.rb
@@ -1,14 +1,15 @@
 module Lita
   module Handlers
     class DeniedBrad < Handler
-     route(/./, :no_brad)
+      route(/./, :no_brad)
 
-     def no_brad
-      return unless request.user.mention_name == "rudi"
+      def no_brad
+        return unless request.user.mention_name == "rudi"
 
-      request.reply_with_mention("Denied Brad!")
+        request.reply_with_mention("Denied Brad!")
+      end
     end
   end
-  
+
   Lita.register_handler(DeniedBrad)
 end
--- lita-denied_brad.gemspec
+++ lita-denied_brad.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = "lita-denied_brad"
-  spec.version       = "0.1.2"
+  spec.version       = "0.1.3"
   spec.authors       = ["Rudikza"]
   spec.email         = ["rudi.kramer@gmail.com"]
   spec.description   = "TODO: Add a description"
