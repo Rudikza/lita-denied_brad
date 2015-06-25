4ba2d0655a74249f4c772a5ac959a86a294b0f6e
Rudikza <rudi.kramer@gmail.com>
2015-06-25 14:59:09 +0200

    Added in denied brad

--- lib/lita/handlers/denied_brad.rb
+++ lib/lita/handlers/denied_brad.rb
@@ -1,8 +1,14 @@
 module Lita
   module Handlers
     class DeniedBrad < Handler
+       route(/./, :no_brad)
     end
 
+    def no_brad
+      return unless request.user.mention_name == "brad"
+
+      request.reply_with_mention("Denied Brad!"
+    end
     Lita.register_handler(DeniedBrad)
   end
 end
